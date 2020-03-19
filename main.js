eeda740e52a80d2f10352b97571d1e817d627971
Your Name <you@example.com>
2020-03-19 09:13:57 -0400

    updates

---
+++ main.js
@@ -0,0 +1,13 @@
+const pinky = document.querySelector(".color-1");
+const paleGoldenrod = document.querySelector(".color-2");
+const yellowGreen = document.querySelector(".color-3");
+const lightblue = document.querySelector(".color-4");
+const white = document.querySelector(".color-5");
+const coloredSquare = document.querySelectorAll(".square");
+
+
+const currentColor = document.querySelector(".current-brush");
+const clickOne = function(){
+    scurrentColor.classList.remove(currentSelection.classList[1]);
+    currentColor.classList.add('color-1');
+};
